f037f692a24c8c835ee54e2b94fff95dee53151a
OMDTZ <omdtanzania@gmail.com>
2022-06-02 13:59:07 +0300

    Modified to create new directory, read files and send all the outputs in the new directory,

--- csv_cleaning.py
+++ csv_cleaning.py
@@ -1,24 +1,46 @@
 import sys, os
 import csv
 
-infile = open(sys.argv[1])
-reader = csv.reader(infile)
-data = list(reader)
-headers=data.pop(0)
+def clean(infile, outfile):
+    reader = csv.reader(open(infile))
+    data = list(reader)
+    headers = data.pop(0)
 
-   
-outfile = open('example.csv','w')
-writer = csv.writer(outfile)
+    of = open(outfile, 'w')
+    writer = csv.writer(of)
 
-for row in data:
-    energy = ''
-    if row[14].lower() == 'grid_luku':
-        energy = 'Electricity'
-    else:
-        energy = row[14]
-    newrow = []
-    newrow.extend(row[0:14])
-    newrow.append(energy)
-    newrow.extend(row[14][15:])
-    writer.writerow(newrow)
+    for row in data:
+        energy = ''
+        if row[14].lower() == 'grid_luku':
+            energy = 'Electricity'
+        else:
+            energy = row[14]
+        newrow = []
+        newrow.extend(row[0:14])
+        newrow.append(energy)
+        newrow.extend(row[14][15:])
+        writer.writerow(newrow)
+
+if __name__ == "__main__":
+    indir = sys.argv[1]
+    filelist = os.listdir(indir)
+    outdir = os.path.join(indir,'CLEANED')
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    for filename in filelist:
+        infile = os.path.join(indir, filename)
+        (filepath, ext) = os.path.splitext(filename)
+        outfile = os.path.join(outdir,filename)
+        try:
+            clean(infile, outfile)
+        except:
+            print('this file is fucked up')
+            print(filename)
+
+    #print(filelist)
+    #clean(sys.argv[1])
+    #newfilename = filepath + '_CLEANED' + ext
+    #dirname = os.path.dirname(filename)
+
+
     
